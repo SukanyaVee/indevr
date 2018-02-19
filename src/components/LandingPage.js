16f6fa79b8a1af0ba5aa22909e9b638846e1c159
Andrea Stringham <andrea@chasingatoms.com>
2018-02-19 11:34:48 -0700

    updates to header

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,9 +4,9 @@ import { connect } from "react-redux";
 import axios from "axios";
 import Auth0Lock from "auth0-lock";
 import glam from "glamorous";
-import SearchBar from "../components/SearchBar";
-import logo from "../assets/LogoMain.png";
-import logo2 from '../assets/LogoMini.png';
+// import SearchBar from "../components/SearchBar";
+// import logo from "../assets/LogoMain.png";
+// import logo2 from '../assets/LogoMini.png';
 import Tasks from "./landing-page-assets/taskboard/tasks";
 import Profile from './landing-page-assets/profile/profile';
 import MidBar from './landing-page-assets/MidBar';
@@ -65,11 +65,11 @@ class LandingPage extends Component {
   render() {
     return (
       <div>
-        <Header>
+        {/* <Header>
           <Logo className='big' src={logo} alt="" />
           <LogoSmall className='small' src={logo2} alt='' />
           <SearchBar />
-        </Header> 
+        </Header>  */}
         <Initial>
           <Main>
             <Heading >
@@ -131,32 +131,32 @@ const Desc = glam.p({
   }
 });
 
-const Logo = glam.img({
-  height: '100%',
-  width: '50%',
-  '@media (max-width: 729px)': {
-    visibility: 'collapse',
-  }
-})
+// const Logo = glam.img({
+//   height: '100%',
+//   width: '50%',
+//   '@media (max-width: 729px)': {
+//     visibility: 'collapse',
+//   }
+// })
 
-const LogoSmall = glam.img({
-  visibility: 'hidden',
-  '@media (max-width: 729px)': {
-    visibility: 'visible',
-    marginLeft: 5,
-  }
-})
+// const LogoSmall = glam.img({
+//   visibility: 'hidden',
+//   '@media (max-width: 729px)': {
+//     visibility: 'visible',
+//     marginLeft: 5,
+//   }
+// })
 
-const Header = glam.header({
-  height: "150",
-  width: "100vw",
-  backgroundColor: 'var(--main-black)',
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "center",
-  marginTop: "0",
-  })
+// const Header = glam.header({
+//   height: "150",
+//   width: "100vw",
+//   backgroundColor: 'var(--main-black)',
+//   display: "flex",
+//   flexDirection: "row",
+//   justifyContent: "space-between",
+//   alignItems: "center",
+//   marginTop: "0",
+//   })
 
 const Heading = glam.h1({
   fontSize: "2.5em",
