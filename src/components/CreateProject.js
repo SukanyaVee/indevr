fe2191ccb2b875b084da61fb202acc8330c2e55f
Andrea Stringham <andrea@chasingatoms.com>
2018-02-24 19:10:34 -0700

    styling for new project form

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -9,7 +9,7 @@ class CreateProject extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            user_id: this.props.user.id, //HARDCODED
+            user_id: 1, //HARDCODED
             proj_id: 0,
             project_name: '',
             description: '',
@@ -27,18 +27,19 @@ class CreateProject extends Component {
     }
 
     createProj(project_name, description, pub, repo) {
-        var newProj = {user_id: this.props.user.id, project_name: project_name, description:description, pub: pub, repo: repo}
+        var newProj = {user_id: this.state.user_id, project_name: project_name, description:description, pub: pub, repo: repo}
         console.log(newProj)
         axios.post('/indevr/projects', newProj).then(resp=>{
             console.log(resp.data)
             this.setState({proj_id: resp.data[0].id})
-            axios.post('/indevr/contributors', {project_id: resp.data[0].id, user_id: this.props.user.id, owner: true}).then(resp=>{ //HARDCODED
+            axios.post('/indevr/contributors', {project_id: resp.data[0].id, user_id: this.state.user_id, owner: true}).then(resp=>{ //HARDCODED
             }).catch(error=>console.log(error))
         }).catch(error=>console.log(error))
         this.setState({showSkillsForm: true})
     }
 
-    createSkill(newSkill,newLevel){
+    createSkill(newSkill,newLevel, e){
+        e.preventDefault()
         var x={project_id: this.state.proj_id, skill: newSkill, level: newLevel}
         console.log(x)
         axios.post('/indevr/skills', x).then(resp=>{
@@ -58,71 +59,111 @@ class CreateProject extends Component {
     }
 
     render() {
-        var mapppp = this.state.stack.map(skill=> 
-            (<ProjectSkills><div key={skill.id}>{skill.skill} - {skill.level===1?'Worthy Warrior':skill.level===2?'Noble Ninja':'Supreme Samurai'}</div></ProjectSkills>)
+        var skills = this.state.stack.map(skill=> {
+            return (
+                <div key={skill.id}>
+                    {skill.skill} - {skill.level === 1 ? 'Worthy Warrior':skill.level === 2 ? 'Noble Ninja':'Supreme Samurai'}
+                </div>
+            )
+        }
+
         )
         return (
-            <Forrrm>
+            <div>
                 {!this.state.showSkillsForm &&
-                <div>
-                Step 1: Create a project. You can add more skills later.<br/><br/>
-                    <input placeholder="Project Name" onChange={e=>{this.setState({project_name: e.target.value})}}/><br/>
-                    <input placeholder="Description"  onChange={e=>{this.setState({description: e.target.value})}}/><br/>
-                    <input placeholder="Link to Github repo" onChange={e=>{this.setState({repo:e.target.value})}}/><br/>
-                    Public?
-                    <select value={this.state.pub} onChange={e=>{this.setState({pub:e.target.value})}}>
-                        <option value={true}>Yes</option>
-                        <option value={false}>No</option>
-                    </select><br/>
-                    <button onClick={e=>{this.createProj(this.state.project_name, this.state.description, this.state.pub, this.state.repo)}}>Create</button>
-                </div>}
+                    <Form>
+                        <label>Step 1: Create a project!</label>
+                        <input className="form-control"
+                            placeholder="Project Name"
+                            onChange={e=>{this.setState({project_name: e.target.value})}}/>
+
+                        <textarea className="form-control"
+                            placeholder="Description"
+                            onChange={e=>{this.setState({description: e.target.value})}}></textarea>
+
+                        <input className="form-control"
+                            placeholder="Link to Github repo"
+                            onChange={e=>{this.setState({repo:e.target.value})}}/>
+
+                        <select className="form-control"
+                            value={this.state.pub}
+                            onChange={e=>{this.setState({pub:e.target.value})}}>
+                            <option value={true}>Public</option>
+                            <option value={false}>Private</option>
+                        </select>
+
+                        <button className="btn"
+                            onClick={e=>{this.createProj(this.state.project_name, this.state.description, this.state.pub, this.state.repo)}}>
+                            Create</button>
+                    </Form>}
+
                 {/* CREATES PROJECT IN projects TABLE, THEN ALLOWS USER TO ENTER SKILLS */}
                 {this.state.showSkillsForm &&
                     <div>
-                        {mapppp}
-                        Add more skills and levels to the project you just created<br/><br/>
-                        <input placeholder="Primary Skill" value={this.state.newSkill} required onChange={e=>{this.setState({newSkill:e.target.value})}}/><br/>
-                        <select value={this.state.newLevel} onChange={e=>{this.setState({newLevel:e.target.value})}}>
-                            <option value="1">Worthy Warrior (novice)</option>
-                            <option value="2">Noble Ninja (intermediate)</option>
-                            <option value="3">Supreme Samurai (advanced)</option>
-                        </select> <br/>
-                        <button onClick={e=>{this.createSkill(this.state.newSkill, this.state.newLevel)}}>Submit and add another skill</button>
-                        <button onClick={e=>{this.completed(this.state.newSkill, this.state.newLevel)}}>Submit and complete</button>
+                        <Form>
+                            <label>Step 2: Build the stack for your project</label>
+                            <input className="form-control"
+                                placeholder="Ex: React"
+                                value={this.state.newSkill}
+                                required onChange={e=>{this.setState({newSkill:e.target.value})}}/>
+
+                            <select className="form-control"
+                                value={this.state.newLevel}
+                                onChange={e=>{this.setState({newLevel:e.target.value})}}>
+                                <option value="1">Worthy Warrior (novice)</option>
+                                <option value="2">Noble Ninja (intermediate)</option>
+                                <option value="3">Supreme Samurai (advanced)</option>
+                            </select>
+                            <button  className="btn"
+                                onClick={e=>{this.createSkill(this.state.newSkill, this.state.newLevel, e)}}>Submit and add another </button>
+                            <button className="btn"
+                                onClick={e=>{this.completed(this.state.newSkill, this.state.newLevel)}}>Submit and complete
+                            </button>
+                        </Form>
+                        <SkillDisplay>
+                            {skills}
+                        </SkillDisplay>
                     </div>}
-            </Forrrm>
+            </div>
         )
     }
 }
 
-const Forrrm = glam.div ({
-    '& input': {
+const Form = glam.form ({
+    padding: 10,
+    maxWidth: 400,
+    textAlign: 'center',
+    '& button':{
+        backgroundColor: 'var(--main-purple)',
+        color: '#fff',
+        width: 'calc(50% - 10px)',
+        fontWeight: 'bold',
         margin: 5,
-        padding: 5,
-        borderRadius: 3
+        ':hover':{
+            color: '#fff'
+        }
     },
-    '& select': {
-        margin: 5,
-        padding: 5,
-        borderRadius: 3
+    '& .form-control':{
+        margin: 10,
+        width: 'calc(100% - 20px)'
     }
 })
 
-const ProjectSkills = glam.div ({
-    padding: 10,
-    borderRadius: 10,
-    '& div': {
-        borderRadius: 3,
-        background: 'var(--main-purple)',
-        // margin: 5,
-        padding:5,
-        color: 'white'
-    },
-    '& span': {
-        fontSize: 10,
-        cursor: 'pointer'
+const SkillDisplay = glam.div({
+    marginTop: 10,
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    '> div':{
+        margin: 5,
+        padding: '3px 5px',
+        color: '#fff',
+        backgroundColor: 'var(--main-purple)',
+        padding: '3px 5px',
+        borderRadius: 5,
     }
 })
+
 
 
 const mapStateToProps = state => {
